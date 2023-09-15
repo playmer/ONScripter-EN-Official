2ab1b79c11a58d545ecd7c9d779ca36aae737700
Joshua T. Fisher <playmer@gmail.com>
2023-09-15 00:45:50 -0700

    Need to finish signing, need to either rip out 7zip or expect it in path

--- android-project/amake/src/main.rs
+++ android-project/amake/src/main.rs
@@ -1,6 +1,6 @@
-use std::{process::Command, path::{Path, PathBuf}, collections::HashMap, io::Write};
+use std::{process::Command, path::{Path, PathBuf}, collections::HashMap, io::Write, fs::OpenOptions};
 use walkdir::WalkDir;
-use zip::write::FileOptions;
+use zip_next::write::FileOptions;
 
 struct Configuration
 {
@@ -77,7 +77,7 @@ fn run_cmake_configure_preset(config : &Configuration, presets : &Vec<(String, S
             .arg(".")
             .current_dir(&config.cmakeliststxt_dir)
             .status()
-            .expect("failed to execute StaticGraphCompilations");
+            .expect("failed to execute cmake.");
 
         if status.code().is_none() || (status.code().unwrap() != 0)
         {
@@ -91,7 +91,7 @@ fn run_cmake_configure_preset(config : &Configuration, presets : &Vec<(String, S
             .arg(&build_preset)
             .current_dir(&config.cmakeliststxt_dir)
             .status()
-            .expect("failed to execute StaticGraphCompilations");
+            .expect("failed to execute cmake.");
 
         if status.code().is_none() || (status.code().unwrap() != 0)
         {
@@ -182,7 +182,7 @@ fn make_apk(config : &Configuration, presets : &Vec<(String, String)>)
     configToAbiMap.insert("android_armv6", "armeabi"); // FIXME
     let configToAbiMap = configToAbiMap;
 
-    let mut status = Command::new(config.android_tools_dir.join("aapt2"))
+    let status = Command::new(config.android_tools_dir.join("aapt2"))
         .arg("compile")
         .arg("-o")
         .arg(config.android_build_dir.join("res.zip"))
@@ -202,8 +202,7 @@ fn make_apk(config : &Configuration, presets : &Vec<(String, String)>)
         std::fs::create_dir_all(native_lib_dir).unwrap();
         std::fs::copy(lib_file_path, lib_file).unwrap();
     }
-    
-    let mut status = Command::new(config.android_tools_dir.join("aapt2"))
+    let status = Command::new(config.android_tools_dir.join("aapt2"))
         .arg("compile")
         .arg("-o")
         .arg(config.android_build_dir.join("native_resources.zip"))
@@ -216,16 +215,16 @@ fn make_apk(config : &Configuration, presets : &Vec<(String, String)>)
 
 
 
-
+    let unaligned_path = config.android_build_dir.join("unaligned.apk");
 
 
 
 
     // Create the initial Apk
-    let mut status = Command::new(config.android_tools_dir.join("aapt2"))
+    let status = Command::new(config.android_tools_dir.join("aapt2"))
         .arg("link")
         .arg("-o")
-        .arg(config.android_build_dir.join("unaligned.apk"))
+        .arg(&unaligned_path)
         .arg("--manifest")
         .arg("AndroidManifest.xml")
         .arg("-I")
@@ -236,29 +235,124 @@ fn make_apk(config : &Configuration, presets : &Vec<(String, String)>)
         .status()
         .expect("Failed to run aapt2 to build initial APK");
 
-    //// Open the unaligned apk file as a zip
-    //let file = std::fs::File::create(Path::new(&config.android_build_dir).join("unaligned.apk")).unwrap();
-    //let mut zip = zip::ZipWriter::new_append(file).unwrap();
-    //// Append classes file onto the apk
-    //let options = FileOptions::default()
-    //    .compression_method(zip::CompressionMethod::Stored)
-    //    .compression_level(None);
-    //zip.start_file_aligned("classes.dex", options, 4).unwrap();
-    //let classes_file = config.android_build_dir.join("classes.dex");
-    //let file_data = std::fs::read(classes_file).unwrap();
-    //zip.write(file_data.as_ref()).unwrap();
-    //// Append the native libraries onto the apk.
-    //for (configure_preset, _build_preset) in presets
-    //{
-    //    let options = FileOptions::default()
-    //        .compression_method(zip::CompressionMethod::Stored)
-    //        .compression_level(None);
-    //    zip.start_file_aligned(format!("lib/{}/libonscripter_en.so", configToAbiMap.get(configure_preset.as_str()).unwrap()), options, 4).unwrap();
-    //    let lib_file = config.cmakeliststxt_dir.join("builds").join(configure_preset).join("lib").join("Release").join("libonscripter_en.so");
-    //    let file_data = std::fs::read(lib_file).unwrap();
-    //    zip.write(file_data.as_ref()).unwrap();
-    //}
-    //zip.finish().unwrap();
+
+
+
+
+
+
+    let status = Command::new("C:/repos/ONScripter-EN-Official/android-project/7za.exe")
+        .current_dir(&config.android_build_dir) // 7zip is cursed and is placing the file in build/fileName without running from the build dir, but only for this.
+        .arg("a")
+        .arg("unaligned.apk")
+        .arg("-tzip")
+        .arg("classes.dex").status().expect("Failed to add to unaligned APK");
+    
+    if status.code().is_none() || (status.code().unwrap() != 0)
+    {
+        println!("Failed to embed classes.dex");
+        return;
+    }
+
+    for (configure_preset, _build_preset) in presets
+    {
+        let native_lib_dir = Path::new("lib").join(&configToAbiMap.get(configure_preset.as_str()).unwrap());
+        let lib_file = native_lib_dir.join("libonscripter_en.so", );
+        let lib_file_path = config.cmakeliststxt_dir.join("builds").join(configure_preset).join("lib").join("Release").join("libonscripter_en.so");
+
+
+        let status = Command::new("C:/repos/ONScripter-EN-Official/android-project/7za.exe")
+            .arg("a")
+            .arg(&unaligned_path)
+            .arg("-tzip")
+            .arg(&lib_file_path).status().expect("Failed to add to unaligned APK");
+        
+        if status.code().is_none() || (status.code().unwrap() != 0)
+        {
+            println!("Failed to embed {}", &native_lib_dir.display());
+            return;
+        }
+        let status = Command::new("C:/repos/ONScripter-EN-Official/android-project/7za.exe")
+            .arg("rn")
+            .arg(&unaligned_path)
+            .arg("-tzip")
+            .arg("libonscripter_en.so")
+            .arg(&native_lib_dir.join("libonscripter_en.so")).status().expect("Failed to rename to in unaligned APK");
+        
+        if status.code().is_none() || (status.code().unwrap() != 0)
+        {
+            println!("Failed to rename native lib to {}", &native_lib_dir.display());
+            return;
+        }
+    }
+
+
+    // FIXME: There's something suble not working here, where we write to the file but it doesn't seem to be added to the archive.
+    // Open the unaligned apk file as a zip
+//    {
+//        let file = OpenOptions::new().read(true).append(true).open(&unaligned_path).unwrap();
+//        let mut zip = zip_next::ZipWriter::new_append(file).unwrap();
+//        zip.set_flush_on_finish_file(true);
+//    
+//        // Append classes file onto the apk
+//        zip.start_file("classes.dex", FileOptions::default()).unwrap();
+//        let classes_file = config.android_build_dir.join("classes.dex");
+//        let file_data = std::fs::read(classes_file).unwrap();
+//        zip.write_all(file_data.as_ref()).unwrap();
+//        
+//        zip.add_directory("lib", Default::default()).unwrap();
+//    
+//        // Append the native libraries onto the apk.
+//        for (configure_preset, _build_preset) in presets
+//        {
+//            zip.add_directory(format!("lib/{}", configToAbiMap.get(configure_preset.as_str()).unwrap()), Default::default()).unwrap();
+//            zip.start_file(format!("lib/{}/libonscripter_en.so", configToAbiMap.get(configure_preset.as_str()).unwrap()), FileOptions::default()).unwrap();
+//            let lib_file = config.cmakeliststxt_dir.join("builds").join(configure_preset).join("lib").join("Release").join("libonscripter_en.so");
+//            let file_data = std::fs::read(lib_file).unwrap();
+//            zip.write_all(file_data.as_ref()).unwrap();
+//        }
+//        zip.finish().unwrap();
+//    }
+
+    println!("Calling Zipalign to produced the aligned APK before signing.");
+    let status = Command::new(config.android_tools_dir.join("zipalign"))
+        .arg("-f")
+        .arg("4")
+        .arg(&unaligned_path)
+        .arg(&config.android_build_dir.join("aligned.apk"))
+        .status()
+        .expect("Failed to Aligned APK");
+    
+    if status.code().is_none() || (status.code().unwrap() != 0)
+    {
+        println!("Failed to align apk.");
+        return;
+    }
+    
+
+    
+
+    println!("Sign the APK");
+    let status = Command::new(config.android_tools_dir.join("apksigner.bat"))
+        .arg("sign")
+        .arg("--ks")
+        .arg("$KEYSTORE")
+        .arg("--ks-pass")
+        .arg("\"pass:$KS_PASS\"")
+        .arg("--min-sdk-version")
+        .arg("15")
+        .arg("--out")
+        .arg("signed.apk")
+        .arg(&config.android_build_dir.join("aligned.apk"))
+        .status()
+        .expect("Failed to sign APK");
+    
+    if status.code().is_none() || (status.code().unwrap() != 0)
+    {
+        println!("Failed to sign apk.");
+        return;
+    }
+
 }
 
 fn main()
@@ -272,13 +366,11 @@ fn main()
     presets.push(("android_x86".to_string(), "android-x86-release".to_string()));
     presets.push(("android_arm64".to_string(), "android-arm64-release".to_string()));
     presets.push(("android_armNeon".to_string(), "android-armNeon-release".to_string()));
-    if !run_cmake_configure_preset(&config, &presets) {
-        println!("Failed to run CMake");
-        return;
-    }
-    
+    //if !run_cmake_configure_preset(&config, &presets) {
+    //    println!("Failed to run CMake");
+    //    return;
+    //}
     std::fs::remove_dir_all(&config.android_build_dir).unwrap_or_default();
-
     run_javac(&config);
     run_d8(&config);
     make_apk(&config, &presets);
